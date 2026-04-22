b5da688ffe90c392562f73334d60880cd189a009
Suprianto Djamalu <ersaka96@gmail.com>
2026-04-22 19:28:57 +0800

    Add pwa & favicon

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'studio-ta-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './icon-512.png'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Event (Network First Strategy)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
